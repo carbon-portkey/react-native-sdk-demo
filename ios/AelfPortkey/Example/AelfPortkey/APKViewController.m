afe50da692d459d551ec0f09646b20014d7c98d9
wade-portkey <codingwizard@portkey.finance>
2023-09-13 15:40:50 +0800

    ios sdk init

---
+++ ios/AelfPortkey/Example/AelfPortkey/APKViewController.m
@@ -0,0 +1,29 @@
+//
+//  APKViewController.m
+//  AelfPortkey
+//
+//  Created by wade-portkey on 09/13/2023.
+//  Copyright (c) 2023 wade-portkey. All rights reserved.
+//
+
+#import "APKViewController.h"
+
+@interface APKViewController ()
+
+@end
+
+@implementation APKViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
